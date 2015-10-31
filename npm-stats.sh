9e311ed08440acbd5c9065c1f720b63bb6752c31
jaxgeller <jacksongeller@gmail.com>
2015-10-31 12:16:11 -0400

    add date logging

--- npm-stats.sh
+++ npm-stats.sh
@@ -5,20 +5,28 @@ npm() {
     start=$(date -u +"%s")
     command npm $@
     end=$(date -u +"%s")
-    difference=$(($end-$start))
+    difference="$(($end-$start))"
     mkdir -p ~/.npm-stats
-    echo "$difference" >> ~/.npm-stats/timing
+    date="$(date '+%Y-%m-%d %H:%M:%S')"
+    echo "$date\t$difference" >> ~/.npm-stats/timing
   else
     command npm $1
   fi
 }
 
 npm-stats() {
+
+  if [ ! -f ~/.npm-stats/timing ]; then
+    echo "No stats to report right now"
+    return 1
+  fi
+
   installs=$(wc -l < ~/.npm-stats/timing | tr -d '[[:space:]]')
   total=0
 
   while read p; do
-    total=$(($total+$p))
+    holder=$(echo "$p" | cut -f2 -d$'\t')
+    total=$(($total+$holder))
   done < ~/.npm-stats/timing
 
   echo "You've run npm install $installs times. For a total of $total seconds."
