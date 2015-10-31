dd1d16e80119f6a21d38688ca5ec80b65657b5ae
jaxgeller <jacksongeller@gmail.com>
2015-10-31 12:28:26 -0400

    add root var

--- npm-stats.sh
+++ npm-stats.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+NPM_STATS_ROOT="$HOME/.npm-stats"
+
 npm() {
   if [ "$1" == "i" ] || [[ $1 == ins* ]]; then
     start=$(date -u +"%s")
     command npm $@
     end=$(date -u +"%s")
     difference="$(($end-$start))"
-    mkdir -p ~/.npm-stats
+    mkdir -p $NPM_STATS_ROOT
     date="$(date '+%Y-%m-%d %H:%M:%S')"
-    echo "$date\t$difference" >> ~/.npm-stats/timing
+    echo "$date\t$difference" >> "$NPM_STATS_ROOT/timing"
   else
     command npm $1
   fi
@@ -16,18 +18,18 @@ npm() {
 
 npm-stats() {
 
-  if [ ! -f ~/.npm-stats/timing ]; then
+  if [ ! -f "$NPM_STATS_ROOT/timing" ]; then
     echo "No stats to report right now"
     return 1
   fi
 
-  installs=$(wc -l < ~/.npm-stats/timing | tr -d '[[:space:]]')
+  installs=$(wc -l < "$NPM_STATS_ROOT/timing" | tr -d '[[:space:]]')
   total=0
 
   while read p; do
     holder=$(echo "$p" | cut -f2 -d$'\t')
     total=$(($total+$holder))
-  done < ~/.npm-stats/timing
+  done < "$NPM_STATS_ROOT/timing"
 
   echo "You've run npm install $installs times. For a total of $total seconds."
 }
